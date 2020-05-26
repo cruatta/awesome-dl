a17e721362d1b6f1d86c03296f0904e33951ca90
Cameron Ruatta <cruatta@users.noreply.github.com>
2020-05-26 00:00:10 -0700

    Task processor and more (#3)
    - Add task processor, removing logic from task classes and make them data classes
    - Refactor for mypy

---
+++ awesomedl/process/task_processor.py
@@ -0,0 +1,38 @@
+from asyncio.subprocess import PIPE, Process, create_subprocess_exec
+from typing import *
+import sys
+
+from awesomedl.config import ConfigManager, ConfigFile
+from awesomedl.model import TaskStatus, TaskType
+from awesomedl.model.task import DownloadTask, YTDLDownloadTask
+from fastapi.logger import logger
+
+
+class TaskProcessor(object):
+
+    def __init__(self, config_manager: ConfigManager):
+        self.config_manager = config_manager
+
+    async def process(self, task: DownloadTask) -> Optional[Process]:
+        if isinstance(task, YTDLDownloadTask):
+            logger.info("is instance YTDL")
+            return await self.ytdl_process(task)
+        else:
+            return None
+
+    async def ytdl_process(self, task: YTDLDownloadTask) -> Optional[Process]:
+        add_args = [task.submitted_task.url, "--newline"]
+
+        config_file: Optional[ConfigFile] = self.config_manager.config(task.type, task.submitted_task.profile)
+
+        if config_file:
+            path = str(config_file.path.resolve())
+            add_args = add_args + ["--ignore-config", "--config-location", path]
+
+        status = TaskStatus(task.submitted_task.status)
+        logger.debug("Task: {} - Status is {}".format(task.submitted_task.uuid, status))
+        if status is not TaskStatus.PROCESSING:
+            return None
+        else:
+            process = await create_subprocess_exec(sys.executable, "-m", "youtube_dl", *add_args, stdout=PIPE)
+            return process
