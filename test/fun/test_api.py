d5904774038aea7589107637da86db512d0f0371
Cameron Ruatta <cameron@ruatta.com>
2020-12-29 18:56:35 -0800

    Functional tests update

--- test/fun/test_api.py
+++ test/fun/test_api.py
@@ -2,17 +2,17 @@ import requests
 
 
 def test_no_api_key():
-    r = requests.get("http://localhost:8080/task/queue", headers={"X-Whatever": "test"})
+    r = requests.get("http://localhost:8080/task/all", headers={"X-Whatever": "test"})
     assert r.ok is False
 
 
-def test_get_queue():
-    r = requests.get("http://localhost:8080/task/queue", headers={"X-ADL-Key": "test"})
+def test_get_all():
+    r = requests.get("http://localhost:8080/task/all", headers={"X-ADL-Key": "test"})
     assert r.ok is True
 
 
 def test_get_ytdl_formats():
-    r = requests.get("http://localhost:8080/ytdl/formats", headers={"X-ADL-Key": "test"})
+    r = requests.get("http://localhost:8080/ytdl/profiles", headers={"X-ADL-Key": "test"})
     assert r.ok is True
 
 
@@ -20,7 +20,8 @@ def test_get_running():
     r = requests.get("http://localhost:8080/task/running", headers={"X-ADL-Key": "test"})
     assert r.ok is True
 
-def test_get_version():
-    r = requests.get("http://localhost:8080/version", headers={"X-ADL-Key": "test"})
+
+def test_get_health():
+    r = requests.get("http://localhost:8080/health", headers={"X-ADL-Key": "test"})
     assert r.ok is True
     assert r.json()["version"] is not ""
