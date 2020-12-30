c596f5505b5fbb6c6e231564bee26acfcf9bbf5b
Cameron Ruatta <cameron@ruatta.com>
2020-12-29 19:53:37 -0800

    Functional tests update

--- test/fun/test_api.py
+++ test/fun/test_api.py
@@ -3,15 +3,15 @@ import requests
 
 def test_e2e():
     r = requests.post("http://localhost:8080/ytdl/task", json={"url": "https://www.youtube.com/watch?v=aqz-KE-bpKQ"},
-                      headers={"X-Whatever": "test"})
+                      headers={"X-ADL-Key": "test"})
     assert r.ok is True
     uuid = r.json()["uuid"]
-    r = requests.get("http://localhost:8080/task/progress/{}".format(uuid), headers={"X-Whatever": "test"})
+    r = requests.get("http://localhost:8080/task/progress/{}".format(uuid), headers={"X-ADL-Key": "test"})
     assert r.ok is True
     assert len(r.json()) == 1
-    r = requests.post("http://localhost:8080/task/cancel", json={"uuid": uuid}, headers={"X-Whatever": "test"})
+    r = requests.post("http://localhost:8080/task/cancel", json={"uuid": uuid}, headers={"X-ADL-Key": "test"})
     assert r.ok is True
-    r = requests.get("http://localhost:8080/task/all", headers={"X-Whatever": "test"})
+    r = requests.get("http://localhost:8080/task/all", headers={"X-ADL-Key": "test"})
     assert r.ok is True
     assert len(r.json()) == 1
     assert r.json()[0]["task"]["status"] == 2
