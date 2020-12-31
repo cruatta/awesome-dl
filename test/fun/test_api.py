73552b9340a79d8934250c778be35f3a0fef1077
Cameron Ruatta <cameron@ruatta.com>
2020-12-30 21:13:54 -0800

    cancel overhaul tests

--- test/fun/test_api.py
+++ test/fun/test_api.py
@@ -18,9 +18,28 @@ def test_e2e_download_cancel():
     assert r.json()[0]["uuid"] == uuid
 
 
+def test_e2e_cancel_all():
+    for i in range(0, 20):
+        r = requests.post("http://localhost:8080/ytdl/task",
+                          json={"url": "https://www.youtube.com/watch?v=aqz-KE-bpKQ"},
+                          headers={"X-ADL-Key": "test"})
+        assert r.ok is True
+        uuid = r.json()["uuid"]
+        r = requests.post("http://localhost:8080/task/cancel/all", headers={"X-ADL-Key": "test"})
+        assert r.ok is True
+        assert len(r.json()) == 1
+        assert r.json()[0]["uuid"] == uuid
+        assert r.json()[0]["success"]
+
+
 def test_no_api_key():
     r = requests.get("http://localhost:8080/task/all", headers={"X-Whatever": "test"})
     assert r.ok is False
+
+
+def test_cancel_all():
+    r = requests.post("http://localhost:8080/task/cancel/all", headers={"X-ADL-Key": "test"})
+    assert r.ok is True
 
 
 def test_get_all():
